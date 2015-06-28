c092f9f4b6beabb65d2e7de6487c8528c88fc1e8
Khem <khem@localhost.localdomain>
2015-06-28 10:33:54 +0100

    testing fix

---
+++ tests/LogTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Khem\Logger\Log;
+
+class LogTest extends PHPUnit_Framework_TestCase
+{
+    public function testIfIsValidHttpRequest()
+    {
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+
+        $this->assertContains($httpdMethod, array('GET', 'POST', 'PUT', 'DELETE'));
+    }
+
+    public function testUserConfig()
+    {
+        //it the user is et
+    }
+
+    public function testIfLogDirectoryExists()
+    {
+        //assertFileExists
+    }
+
+    public function testIfLogDirectoryIsWritable()
+    {
+
+    }
+
+}
